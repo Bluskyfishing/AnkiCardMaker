f89827f3bc54313737f4201ae10a0918fabf9d39
Bluskyfishing <blusky445@gmail.com>
2024-03-14 20:18:23 +0100

    improved string readability.

--- webScraperTest/Program.cs
+++ webScraperTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using System.Dynamic;
 using System.Text;
 
 namespace webScraperTest
@@ -11,36 +12,46 @@ namespace webScraperTest
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
 
-            //Kanji to search for:
-            Console.WriteLine("Input Kanji/Kanji word:");
-            String kanji = Console.ReadLine();
-
-            //Get request jisho.org
-            Console.WriteLine(kanji);
-            string url = $"https://jisho.org/word/{kanji}";
-            Console.WriteLine(url);
-            var httpClient = new HttpClient();
-            var hmtl = httpClient.GetStringAsync(url).Result;
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(hmtl);
-
-            //Get furigana
-            var furiganaElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='furigana']");
-            var furigana = furiganaElement.InnerText.Trim();
-            Console.WriteLine(furigana);
-
-            //Get meaning 
-            //var meaningElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='meaning-meaning']");
-            //var meaning = meaningElement.InnerText.Trim();
-            //Console.WriteLine(meaning);
-
-            var meaningNodes = htmlDocument.DocumentNode.SelectNodes("//span[@class='meaning-meaning']");
-
-            foreach( var node in meaningNodes)
+            while (true)
             {
-                Console.WriteLine(node.InnerHtml);
-                Console.WriteLine();
+                //Kanji to search for:
+                Console.WriteLine("Input Kanji/Kanji word:");
+                String kanji = Console.ReadLine();
+
+                if (kanji == "stop")
+                {
+                    break;
+                }
+
+                //Get request jisho.org
+                string url = $"https://jisho.org/word/{kanji}";
+                Console.WriteLine(url);
+                var httpClient = new HttpClient();
+                var hmtl = httpClient.GetStringAsync(url).Result;
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(hmtl);
+
+                //Get furigana
+                var furiganaElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='furigana']");
+                var furigana = furiganaElement.InnerText.Trim();
+
+                Console.WriteLine($"Furigana: {furigana}");
+
+                //Get Meaning(s)
+                var meaningNodes = htmlDocument.DocumentNode.SelectNodes("//span[@class='meaning-meaning']");
+                int index = 1;
+
+                foreach (var node in meaningNodes)
+                {
+                    Console.WriteLine($"Meaning: {index}. {node.InnerHtml}\n");
+                    index++;
+                }
+
+                //Get tags
+
+                //Get example sentence
             }
+
 
         }
     }
