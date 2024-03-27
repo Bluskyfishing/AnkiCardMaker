797b180f70abdeee379fe865f426ed6628c9dff2
Bluskyfishing <blusky445@gmail.com>
2024-03-27 23:08:17 +0100

    Added exception handling for kanji without tags. + Removed some unused code.

--- AnkiCardMaker/Program.cs
+++ AnkiCardMaker/Program.cs
@@ -58,7 +58,7 @@ namespace AnkiCardMaker
                             writer.Write('\n');
                         }
                     }
-                    Console.WriteLine("File created successfully! " + filePath);
+                    Console.WriteLine($"File created successfully!{filePath}\nWrote: {kanjiInfo.Count} Kanji to file.");
                     fs.Close();
                 }
             }
@@ -88,11 +88,9 @@ namespace AnkiCardMaker
             }
             catch (AggregateException)
             {
-                //Console.Clear();
                 Console.WriteLine($"Unable to find kanji:{kanji}");
                 return new List<string[]>();
             }
-            //Console.Clear();
 
             //Get JMdict ID 
             var JMdictIDNodes = htmlDocument.DocumentNode.SelectNodes("//ul[@class='f-dropdown']/li/a");
@@ -136,43 +134,48 @@ namespace AnkiCardMaker
             kanjiInfo[3] = meanings;
 
             //Get Tags 
-            var tagsNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='meaning-tags']");
-            List<string> tagsList = new List<string>();
-            string[] tagsBlackList = ["Wikipedia definition", "verb-Other", "Other forms", "Notes"];
 
-            foreach (var node in tagsNodes)
+            try
             {
-                if (tagsBlackList.Contains(node.InnerText)) { continue; }
-                if (node.InnerText.Length > 35)
-                {
-                    if (node.InnerText.Substring(0, 11) == "Expressions")
-                    {
-                        string expressionString = node.InnerText.Substring(0, 10) + " " + node.InnerText.Substring(36) + " ";
-                        string expression = expressionString.Replace(", ", "");
+                var tagsNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='meaning-tags']");
+                List<string> tagsList = new List<string>();
+                string[] tagsBlackList = ["Wikipedia definition", "verb-Other", "Other forms", "Notes"];
 
-                        tagsList.Add(expression);
-                        continue;
+                foreach (var node in tagsNodes)
+                {
+                    if (tagsBlackList.Contains(node.InnerText)) { continue; }
+                    if (node.InnerText.Length > 35)
+                    {
+                        if (node.InnerText.Substring(0, 11) == "Expressions")
+                        {
+                            string expressionString = node.InnerText.Substring(0, 10) + " " + node.InnerText.Substring(36) + " ";
+                            string expression = expressionString.Replace(", ", "");
+
+                            tagsList.Add(expression);
+                            continue;
+                        }
+
                     }
 
+                    string[] tagSplit = node.InnerText.Split(", ");
+
+                    for (int i = 0; i < tagSplit.Length; i++)
+                    {
+                        string noSpacetag = tagSplit[i].Replace(" ", "_"); //separates space with "_"
+                        tagsList.Add(noSpacetag + " ");
+                    }
                 }
 
-                string[] tagSplit = node.InnerText.Split(", ");
+                string tagsString = string.Join("", tagsList);
+                string tags = tagsString.Replace("&#39;", "'"); //fixes weird unicode bug.
 
-                for (int i = 0; i < tagSplit.Length; i++)
-                {
-                    string noSpacetag = tagSplit[i].Replace(" ", "_"); //separates space with "_"
-                    tagsList.Add(noSpacetag + " "); 
-
-                }
+                kanjiInfo[4] = tags;
             }
-
-            string tagsString = string.Join("", tagsList);
-            string tags = tagsString.Replace("&#39;", "'"); //fixes weird unicode bug.
-
-            kanjiInfo[4] = tags;
-
-            //print of array:
-            //foreach (string s in kanjiInfo) { Console.WriteLine($"kanjiInfo:{s}"); }
+            catch (NullReferenceException e)   
+            {
+                Console.WriteLine("Couldnt find Tags! Exeption: " + e);
+                kanjiInfo[4] = "NOTAG";
+            }
 
             //Check if everything is in list.
             if (kanjiInfo.Length == 5)
